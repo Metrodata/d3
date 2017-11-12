fe58564e1c57a0de0f879fd5cf74f671adb85f6f
Liu Cong <1434580150@qq.com>
2017-11-12 23:26:00 +0800

    lesson 3

--- lesson-2/script.js
+++ lesson-2/script.js
@@ -1,22 +1,102 @@
 window.onload = function(){
 
-d3.select("body").append("svg").attr("width",600).attr("height",800)
 
-let margin = {top: 40, right: 40, bottom: 40, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+let width = 960,
+	height = 2000;
+
+let svg = d3.select("body")
+			.append("svg")
+			.attr("id","canvas")
+			.attr("width",width)
+			.attr("height",height)
+
+let g = svg.append("g").attr("transform", "translate(40,0)");
+
+let stratify = d3.stratify()
+    .parentId(function(d) {
+    	// console.log(d)
+    	// console.log(d.name.substring(0, d.name.lastIndexOf(".")))
+    	return d.name.substring(0, d.name.lastIndexOf(".")); 
+    })
+    .id(function(d){
+    	return d.name
+    });
 
 
-let xScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([0, width]);
+let scaleLinear = d3.scaleLinear().range([0,.001])
 
-let yScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([height, 0]);
+let colorScaleLinear = d3.scaleLinear().range(["red","royalblue"])
 
-var xAxis = d3.axisBottom(xScale)
+console.log(scaleLinear(1))
 
-var yAxis = d3.axisLeft(yScale)
+
+
+
+d3.csv("./data.csv", function(error, data) {
+	if (error) throw error;
+
+	let _root = stratify(data)
+
+	d3.cluster().size([height, width - 160])(_root);
+
+	console.log(_root)
+	// console.log(cluster_root)
+
+
+	console.log(_root.descendants().slice(1), "=========")
+	console.log(_root.descendants())
+
+
+	let link = g.selectAll(".link")
+		.data(_root.descendants().slice(1))
+		.enter()
+		.append("path")
+		.attr("class", "link")
+		.attr("d", function(d) {
+			return "M" + d.y + "," + d.x
+			    + "C" + (d.parent.y + 100) + "," + d.x
+			    + " " + (d.parent.y + 100) + "," + d.parent.x
+			    + " " + d.parent.y + "," + d.parent.x;
+		})
+		.attr("fill","none")
+		.attr("stroke","#555");
+
+
+	var node = g.selectAll(".node")
+		.data(_root.descendants())
+		.enter().append("g")
+		.attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+		.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+		
+	node.append("circle")
+		.attr("r", function(d){
+			return scaleLinear(d.data.value)
+		})
+		.attr("fill",function(d){
+			return colorScaleLinear(scaleLinear(d.data.value))
+		});
+		
+	node.append("text")
+		.attr("dy", 3)
+		.attr("x", function(d) { return d.children ? -8 : 8; })
+		.style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+		.text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+
+
+// console.log(node)
+
+// .link {
+//   fill: none;
+//   stroke: #555;
+//   stroke-opacity: 0.4;
+//   stroke-width: 1.5px;
+// }
+
+});
 
 }
+
+
+
+
+
